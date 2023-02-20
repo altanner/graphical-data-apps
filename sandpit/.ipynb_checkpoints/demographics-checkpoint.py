8d71975132bf3cb1d95ba7c83103bc2d5099ffa2
alleetanner <8190834+alleetanner@users.noreply.github.com>
2023-02-20 16:56:20 +0000

    update and deploy

--- sandpit/.ipynb_checkpoints/demographics-checkpoint.py
+++ sandpit/.ipynb_checkpoints/demographics-checkpoint.py
@@ -1,68 +1,119 @@
-# streamlit will create the user interface
 import streamlit as st
-# pandas will organise my data
 import pandas as pd
-# plotly will make pretty charts
 import plotly.express as px
 
-# use pandas to read my csv file into a dataframe
-#demographics_df = pd.read_csv("demographics.csv")
-demographics_df = px.data.gapminder()
+st.set_page_config(
+    page_title="A Demographics Data App",
+    layout="wide")
+
+# use pandas to read csv file into a dataframe
+demographics_df = pd.read_csv("demo_dataset.csv")
+#demographics_df = pd.read_csv("demo2.csv")
+
+#demo2 = pd.read_csv("demo3.csv")
+
 
 # for my csv
-#column_names = ["country","continent","year","life_exp","hdi_index","co2_consump","gdp","services"]
-# for px.data.gapminder
-column_names = ["country", "continent", "year", "lifeExp", "pop", "gdpPercap", "iso_alpha", "iso_num"]
+column_names = ["Life expectancy","HDI index","CO2 per capita","GDP per capita"]
 
-st.set_page_config(layout="wide")
+#column_names = ["Country", "Continent", "Year", "Life expectancy", "Population", "GDP per capita"]
 
-st.sidebar.image("globe.png")
-st.sidebar.header("Demographic Data")
-st.sidebar.write("Data explorer app")
+with st.sidebar:
+    st.image("globe.png")
+    st.header("Demographic Data")
+    st.info("Welcome to the global demographic explorer data app.")
+    st.checkbox(
+        label="Show data preview",
+        help="If selected, display the data that is being used.")
+    year = st.slider(
+        label="Year",
+        min_value=1998,
+        max_value=2018,
+        value=2008)
+    x_data = st.radio(
+        label="X-axis data",
+        options=column_names)
+    y_data = st.radio(
+        label="Y-axis data",
+        options=column_names,
+        index=1)
+    size_data = st.radio(
+        label="Size data",
+        options=["CO2 per capita", "GDP per capita"])
+    size_scale = st.slider(
+        label="Size scale",
+        min_value=10,
+        max_value=90,
+        value=30)
+
+    
 with st.expander(label="World Demographics Data Explorer: click for instructions"):
-    st.info("""##### World Demographics Data Explorer
+    st.info("""World Demographics Data Explorer
     - This app explores information about social, economic and environmental development at local, national and global levels.
     - Please use the options in the sidebar to explore the dataset.
     - Draw a box to zoom on the chart. Return to normal zoom with a double-click.""")
 
-st.sidebar.checkbox(
-    label="Show data preview",
-    help="If selected, display the data that is being used.",
-    key="data_preview")
+#chart_1 = px.scatter(
+##    demographics_df.query(f"Year=={year}"),
+#    y="GDP per capita",
+##    x="Life expectancy",
+#    size="Population",
+#    size_max=60,
+#    color="Continent",
+#    hover_name="Country",
+#    log_y=True)
 
-st.sidebar.radio(
-    label="X-axis data",
-    options=column_names,
-    key="x_data")
 
-st.sidebar.radio(
-    label="Y-axis data",
-    options=column_names,
-    key="y_data") 
+#chart_1 = px.scatter(
+#    demographics_df,
+#    x=x_data,
+#    y=y_data,
+#    animation_frame="Year",
+#    animation_group="Country",
+#    size="Population",
+#    color="Continent",
+#    hover_name="Country",
+#    log_x=True,
+#    log_y=True,
+#    size_max=64,
+#    range_x=[100,100000],
+#    range_y=[25,90])
 
-if st.session_state.data_preview == True:
-    st.dataframe(demographics_df)
+chart_2 = px.scatter(
+    demographics_df,
+    x=x_data,
+    y=y_data,
+    size=size_data,
+    size_max=size_scale,
+    color="Continent",
+    animation_frame="Year",
+    animation_group="Country",
+    hover_name="Country",
+    height=700)
+
+
+
+
+
+#st.plotly_chart(chart_1, use_container_width=True)
+st.plotly_chart(chart_2, use_container_width=True)
+
+
 
 #chart = px.scatter(
 #    demographics_df,
-#    x=st.session_state.x_data,
-#    y=st.session_state.y_data,
-#    color="continent")
-
-
-chart = px.scatter(
-    demographics_df,
-    x=st.session_state.x_data,
-    y=st.session_state.y_data,
-    animation_frame="year",
-    animation_group="country",
-    size="pop",
-    color="continent",
-    hover_name="country",
-    log_x=True,
-    size_max=55,
-    range_x=[100,100000],
-    range_y=[25,90])
+#    x=x_data,
+#    y=y_data,
+#    animation_frame="Year",
+#    animation_group="Country",
+#    size="Population",
+#    color="Continent",
+#    hover_name="Country",
+#    log_x=True,
+#    log_y=True,
+#    size_max=64,
+#    range_x=[100,100000],
+#    range_y=[25,90])
 
 
 #chart = px.scatter(
@@ -81,20 +132,8 @@ chart = px.scatter(
 #    dimensions=column_names,
 #    color="continent")
 
-st.plotly_chart(chart, use_container_width=True)
 
 
-
-#chart = px.parallel_coordinates(
-#    wine_dataframe,
-#    dimensions=column_names,
-#    #color="class",
-#    color_continuous_scale=px.colors.diverging.Tealrose)
-
-#chart = px.violin(
- #   wine_dataframe,
-  #  x="quality",
-   # y="alcohol",)
 
 
 
