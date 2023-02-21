9aab41c390af5e29388d73eaf663070df3e49497
alleetanner <8190834+alleetanner@users.noreply.github.com>
2023-02-21 17:50:53 +0000

    update and deploy

--- sandpit/.ipynb_checkpoints/demographics-checkpoint.py
+++ sandpit/.ipynb_checkpoints/demographics-checkpoint.py
@@ -12,21 +12,20 @@ demographics_df = pd.read_csv("demo_dataset.csv")
 # for my csv
 column_names = ["Life expectancy", "HDI index", "CO2 per capita", "GDP per capita"]
 
-# build the instructions box
-with st.expander(label="World Demographics Data Explorer: click for instructions"):
-    st.info("""World Demographics Data Explorer
-    - This app explores information about social, economic and environmental development at local, national and global levels.
-    - Please use the options in the sidebar to explore the dataset.
-    - Draw a box to zoom on the chart. Return to normal zoom with a double-click.""")
-
 # build the sidebar
 with st.sidebar:
+    
+    # put a title in the main page body
+    st.title("World Demographics")
+    # add an image
     st.image("globe.png")
-    st.header("Demographic Data")
-    st.info("Welcome to the global demographic explorer data app.")
-    st.checkbox(
-        label="Show data preview",
-        help="If selected, display the data that is being used.")
+    # add a blue info box 
+    st.info("Welcome to the global demographic data explorer app.")
+
+    # visualisation type checkbox
+    animate_vis = st.checkbox(
+        label="Animate",
+        value=False)
     
     # year slider widget
     year = st.slider(
@@ -45,58 +44,45 @@ with st.sidebar:
         index=1)
     
     # balloon size widgets
-    size_data = st.radio(
+    balloon_size_data = st.radio(
         label="Size data",
         options=["CO2 per capita", "GDP per capita"])
-    size_scale = st.slider(
+    balloon_size_scale = st.slider(
         label="Size scale",
         min_value=10,
         max_value=90,
         value=30)
 
-#chart_1 = px.scatter(
-##    demographics_df.query(f"Year=={year}"),
-#    y="GDP per capita",
-##    x="Life expectancy",
-#    size="Population",
-#    size_max=60,
-#    color="Continent",
-#    hover_name="Country",
-#    log_y=True)
+# end of sidebar!
 
-#chart_1 = px.scatter(
-#    demographics_df,
-#    x=x_data,
-#    y=y_data,
-#    animation_frame="Year",
-#    animation_group="Country",
-#    size="Population",
-#    color="Continent",
-#    hover_name="Country",
-#    log_x=True,
-#    log_y=True,
-#    size_max=64,
-#    range_x=[100,100000],
-#    range_y=[25,90])
-
-#st.plotly_chart(chart_1, use_container_width=True)
-
-
-# use plotly.express to build our animated chart object
-animated_chart = px.scatter(
-    demographics_df,
-    x=x_data,
-    y=y_data,
-    size=size_data,
-    size_max=size_scale,
-    color="Continent",
-    animation_frame="Year",
-    animation_group="Country",
-    hover_name="Country",
-    height=700)
+# check if user wants an animation or static visual
+if animate_vis == False:
+    # use plotly.express to create static chart object
+    chart = px.scatter(
+        demographics_df.query(f"Year=={year}"),
+        y="GDP per capita",
+        x="Life expectancy",
+        color="Continent",
+        hover_name="Country",
+        size="GDP per capita",
+        size_max=20,
+        height=650)
+else:
+    # use plotly.express to create our animated chart object
+    chart = px.scatter(
+        demographics_df,
+        x=x_data,
+        y=y_data,
+        color="Continent",
+        hover_name="Country",
+        size=balloon_size_data,
+        size_max=balloon_size_scale,
+        animation_frame="Year",
+        animation_group="Country",
+        height=650)
 
 # ask streamlit to display animated_chart
-st.plotly_chart(animated_chart, use_container_width=True)
+st.plotly_chart(chart, use_container_width=True)
 
 
 
